2dfef26e0fd41c796c16d7872e514406085b7464
Nacho <revuelta.ig@gmail.com>
2025-03-24 16:11:35 +0100

    feat: added searchbar

---
+++ src/components/shared/SearchBar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface Props {
+  modelValue: string
+  placeholder?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  placeholder: 'Filter...',
+})
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<template>
+  <div class="rounded-xl p-4 bg-secondary border-b border-secondary-light">
+    <Input
+      :model-value="modelValue"
+      type="search"
+      icon-name="magnifier"
+      :placeholder="placeholder"
+      @update:model-value="(value: any) => emit('update:modelValue', value)"
+    />
+  </div>
+</template>
